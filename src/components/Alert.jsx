40a4d3ded0b8a7d4844d6f42d1a9469b31805411
Malaika-Ali <malaika10ali@gmail.com>
2024-02-22 18:29:24 +0500

    set font styling in Admin Home Page

--- src/components/Alert.jsx
+++ src/components/Alert.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Alert({ bgColor50, textColor900,iconSrc, iconColor, message, alertsNumber, borderColor600, textColor500,borderColor500, onClick }) {
   return (
-    <div className={`ml-auto mr-3 flex flex-col gap-2 w-60 border-b-4 ${borderColor600} cursor-pointer`} onClick={onClick}>
+    <div className={`ml-auto mr-3 flex flex-col gap-2 w-60 border-b-4 ${borderColor600} cursor-pointer lato-regular-16 font-bold`} onClick={onClick}>
     <div className={`${bgColor50} rounded-t ${textColor900} px-4 py-3`} role="alert">
       <div className="flex flex-row items-center justify-between">
         <div>
@@ -11,7 +11,8 @@ function Alert({ bgColor50, textColor900,iconSrc, iconColor, message, alertsNumb
         <div>
           <p className={`text-sm ${textColor500}`}>{message}</p>
         </div>
-          <span className={`${textColor500} text-xl p-1 rounded-full border ${borderColor500}`}>{alertsNumber}</span>
+          {/* <span className={`${textColor500} text-xl p-2 rounded-full border ${borderColor500} text-xs`}>{alertsNumber}</span> */}
+          <div className={`${textColor500} flex justify-center items-center text-xl h-6 w-6 p-2 rounded-full border ${borderColor500}`}><span className='text-sm'>{alertsNumber}</span> </div>
       </div>
     </div>
   </div>
